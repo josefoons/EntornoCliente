bf6ab246083630a8a27ca2932c857c6858983726
Jose Fons <josefoons@gmail.com>
2018-09-20 19:33:47 +0200

    update js Ejercicio1

--- Tema 1/js/ejercicio1.js
+++ Tema 1/js/ejercicio1.js
@@ -1,9 +1,14 @@
 var arr = new Array();
 var tamanyo = prompt("Dime el tamanyo del array.");
+var mayor = -1;
 
 for (let index = 0; index < tamanyo; index++) {
-    var name = prompt("Introduce un numero");
-    arr.push(name);
+    var numero = prompt("Introduce un numero");
+    arr.push(numero);
+
+    if(numero > mayor) {
+        mayor = numero;
+    }
 }
 
-document.write(arr);
+document.write("El mayor es " + mayor);
