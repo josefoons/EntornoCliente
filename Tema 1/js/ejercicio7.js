871eb8a1d853d4269f59f8b137c3d5b75fbcb7f1
Jose Fons <josefoons@gmail.com>
2018-09-25 18:15:26 +0200

    Upload Ejercicio 7

---
+++ Tema 1/js/ejercicio7.js
@@ -0,0 +1,14 @@
+
+var maximo = prompt("Dime altura");
+
+for (let i = 1; i <= maximo; i++) {
+
+    for (let j = 0; j < i; j++) {
+
+        document.write("* ");
+
+    }
+
+    document.write("<br>");
+
+}
