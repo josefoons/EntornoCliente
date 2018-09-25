d7fc8b5add87a20e9496a0d53439d9043bab6d8d
Jose Fons <josefoons@gmail.com>
2018-09-25 19:32:11 +0200

    Update Ejercicio2

--- Tema 1/js/ejercicio2.js
+++ Tema 1/js/ejercicio2.js
@@ -1,4 +1,4 @@
-var arr = new Array();
+/* var arr = new Array();
 var inicio = 1;
 
 for (let index = 0; index < 5; index++) {
@@ -15,4 +15,30 @@ for (let index = 0; index < 5; index++) {
     document.write(suma + "<br>");
     inicio = suma;
 
+} */
+
+function beHappy(value) {
+
+    let repeat = [];
+
+    while (value > 1 && !~repeat.indexOf(value)) {
+        let result = 0;
+
+        repeat.push(value);
+
+        for (let n of value.toString().split("")) {
+
+            result += Math.pow(n, 2)
+        }
+
+        value = result;
+    }
+
+    return value == 1;
+}
+
+for (let i = 0; i <= 20; i++) {
+    if (beHappy(i)) {
+        document.write(i + "<br>")
+    }
 }
