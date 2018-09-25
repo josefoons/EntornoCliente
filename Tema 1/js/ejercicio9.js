055839a2bc14854236e67e80ccb3f161956f0d65
Jose Fons <josefoons@gmail.com>
2018-09-25 19:01:11 +0200

    Update Ejercicio 9

--- Tema 1/js/ejercicio9.js
+++ Tema 1/js/ejercicio9.js
@@ -1,9 +1,17 @@
 function protegerEmail(email) {
     
+    var correoPartido = email.split("@");
+    var parteTexto = correoPartido[0].split("");
 
-    
+    document.write(parteTexto[0]);
+
+    for (let index = 1; index < parteTexto.length - 1; index++) {
+        
+        document.write("#");
+        
+    }
+    return parteTexto[parteTexto.length - 1] + "@" + correoPartido[1];
 }
 
 
-
-document.write(protegerEmail("diego.garcia@ieslasenia.org")
+document.write(protegerEmail("diego.garcia@ieslasenia.org"));
