27304cf72c0fb42dfe8d52fb1a5934c0cb4d2783
Jose Fons <josefoons@gmail.com>
2018-09-25 17:48:31 +0200

    Ejercicio 4

--- Tema 1/js/ejercicio4.js
+++ Tema 1/js/ejercicio4.js
@@ -1,18 +1,7 @@
-function hola (input){
-    var reg = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
-    if (input.match(reg)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+var fecha = new Date();
 
-
-var usuario = prompt("Fecha pls");
-var cosa = usuario.split("/");
-var dt = new Date(cosa[0] + '/' + cosa[1] + '/' + cosa[2]);
-
-
- if (hola(usuario)) {
-     document.write("yes");
- }
+if (fecha instanceof Date && !isNaN(fecha.valueOf())) {
+  document.write("Variable tipo date");
+} else {
+  document.write("Variable no tipo date");
+}
