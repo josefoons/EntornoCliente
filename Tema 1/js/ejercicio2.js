b0220f1c438621ddbf2c1933d0e0a105de0771ac
Jose Fons <josefoons@gmail.com>
2018-09-27 15:55:35 +0200

    Update Ejercicio 2

--- Tema 1/js/ejercicio2.js
+++ Tema 1/js/ejercicio2.js
@@ -1,21 +1,3 @@
-/* var arr = new Array();
-var inicio = 1;
-
-for (let index = 0; index < 5; index++) {
-    
-
-    arr = inicio.toString().split("");
-    var suma = 0;
-
-    for (let index = 0; index < arr.length; index++) {
-        var suma = Math.pow(arr[index],2) + suma;
-    }
-
-    arr = new Array();
-    document.write(suma + "<br>");
-    inicio = suma;
-
-} */
 
 function hazmeFeliz(value) {
 
